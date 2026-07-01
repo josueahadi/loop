7c618416b996f52df473ad4e7d4e61101485350a
Habib Josue Ahadi <ahadihjosue@gmail.com>
2026-07-01 16:13:25 +0200

    chore(mobile): regenerate iOS/plugin registrations after dependency changes
    Reflects dio + flutter_secure_storage added and font_awesome removed (plugin
    registrants, Podfile.lock, project.pbxproj, SPM Package.resolved).

--- mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,7 +11,6 @@ import file_selector_macos
 import firebase_core
 import firebase_storage
 import flutter_secure_storage_macos
-import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -20,5 +19,4 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
